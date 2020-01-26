73cc33abbe1152cf742761df70d8d0bb6e7d03ac
prajna <prajnabhat111@gmail.com>
2020-01-26 13:32:40 +0530

    this is student repo

---
+++ app/src/main/java/com/example/student/tuser.java
@@ -0,0 +1,17 @@
+package com.example.student;
+
+public class tuser {
+    public String name, email, tid, sub1;
+    final public int type=2;
+
+    public tuser() {
+    }
+
+    public tuser(String name, String email, String tid, String sub1,int type) {
+        this.name = name;
+        this.email = email;
+        this.tid = tid;
+        this.sub1 = sub1;
+        //this.sub2 = sub2;
+    }
+}
---
+++ app/src/main/java/com/example/student/user.java
@@ -0,0 +1,14 @@
+package com.example.student;
+
+public class user {
+    public String name, email, usn, sec, sem;
+    final public int type=1;
+
+    public user(String name, String email, String usn, String sec, String sem,int type) {
+        this.name = name;
+        this.email = email;
+        this.usn = usn;
+        this.sec = sec;
+        this.sem = sem;
+    }
+}
